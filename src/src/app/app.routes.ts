82a49cc844e5ff1fa50c241b1e6eaf1977c4667a
Mateusz Piotr Moruś <mpmorus@gmail.com>
2023-12-13 10:38:35 +0100

    Moved angular project files up

--- src/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import {Routes} from '@angular/router';
-import {HappinessComponent} from "./components/happiness/happiness.component";
-import {ResearchComponent} from "./components/research/research.component";
-
-export const routes: Routes = [
-  {path: 'happiness', component: HappinessComponent},
-  {path: 'research', component: ResearchComponent},
-  {path: '**', redirectTo: '/happiness', pathMatch: 'full'}
-];
